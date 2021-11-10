dde5a2d6b5b4c04b2016b43c04505694431a88d2
6Whistle <78692304+6Whistle@users.noreply.github.com>
2021-11-10 12:57:20 +0900

    Add files via upload

---
+++ Assignment2-main/AVLTree.h
@@ -0,0 +1,29 @@
+#ifndef _AVLTREE_H_
+#define _AVLTREE_H_
+
+#include "VaccinationData.h"
+#include "AVLNode.h"
+/* class AVL */
+
+class AVLTree{
+private:
+	AVLNode*	root;
+
+public:
+	
+	AVLTree(){
+		root=NULL;
+	}
+	~AVLTree(){
+
+	};
+	bool		Insert(VaccinationData* pVac);
+	VaccinationData*	Search(string name);
+	void GetVector(vector<VaccinationData*>& v);
+	int compare_string(string a, string b);
+	void DeleteTree(void);
+
+};
+
+#endif
+
